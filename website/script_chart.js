65ff6cbf581b20a3e29f605cfd064b135f437baa
Thomas Camminady <0milieux_member@icloud.com>
2024-01-31 09:02:26 +0100

    completely refactor and change layout

--- website/script_chart.js
+++ website/script_chart.js
@@ -4,7 +4,6 @@ const dataUrl =
 fetch(dataUrl)
     .then((response) => response.text())
     .then((data) => {
-
         const parsedData = d3.csvParse(data, d3.autoType);
         const newestTimestamp = d3.max(parsedData, (d) =>
             new Date(d.datetime).getTime()
@@ -24,48 +23,64 @@ fetch(dataUrl)
             d.kph = d.distance_m / 1000 / (d.duration_in_traffic_s / 3600);
             d.ziel = "Ziel: " + d.to;
         });
-
-        const fromValues = Array.from(new Set(parsedData.map((d) => d.from)));
-        fromValues.forEach((fromValue) => {
-            createObservablePlotChart1(parsedData, fromValue);
+        const criterias = Array.from(new Set(parsedData.map((d) => d.route)));
+        criterias.forEach((criteria) => {
+            createObservablePlotChart(
+                parsedData,
+                criteria,
+                "kph",
+                "#chart_kph",
+                "Reisegeschwindigkeit (km/h)",
+                [10, 50]
+            );
         });
-        fromValues.forEach((fromValue) => {
-            createObservablePlotChart2(parsedData, fromValue);
+        criterias.forEach((criteria) => {
+            createObservablePlotChart(
+                parsedData,
+                criteria,
+                "durationInTrafficMinutes",
+                "#chart_time",
+                "Reisezeit (min)",
+                [2, 8]
+            );
         });
 
-
-
-        document.getElementById('btnChart1').addEventListener('click', function () {
-            document.getElementById('chart_time').style.display = 'block';
-            document.getElementById('chart_kph').style.display = 'none';
-            document.getElementById('btnChart1').classList.add('active');
-            document.getElementById('btnChart2').classList.remove('active');
+        document.getElementById("btnChart1").addEventListener("click", function () {
+            document.getElementById("chart_time").style.display = "block";
+            document.getElementById("chart_kph").style.display = "none";
+            document.getElementById("btnChart1").classList.add("active");
+            document.getElementById("btnChart2").classList.remove("active");
         });
 
-        document.getElementById('btnChart2').addEventListener('click', function () {
-            document.getElementById('chart_time').style.display = 'none';
-            document.getElementById('chart_kph').style.display = 'block';
-            document.getElementById('btnChart1').classList.remove('active');
-            document.getElementById('btnChart2').classList.add('active');
+        document.getElementById("btnChart2").addEventListener("click", function () {
+            document.getElementById("chart_time").style.display = "none";
+            document.getElementById("chart_kph").style.display = "block";
+            document.getElementById("btnChart1").classList.remove("active");
+            document.getElementById("btnChart2").classList.add("active");
         });
 
         document.getElementById("btnChart1").classList.add("active");
     })
     .catch((error) => console.error("Error fetching the data:", error));
 
-function createObservablePlotChart1(data, fromValue) {
-    const filteredData = data.filter((d) => d.from === fromValue && d.parsedTime < d3.timeParse("%H:%M:%S")(
-        "22:00:00"
-    ));
+function createObservablePlotChart(
+    data,
+    criteria,
+    field,
+    chartname,
+    label,
+    domain
+) {
+    const filteredData = data.filter(
+        (d) =>
+            d.route === criteria &&
+            d.parsedTime < d3.timeParse("%H:%M:%S")("22:00:00")
+    );
     const filteredDataToday = filteredData.filter((d) => d.is_today);
-
-    // Group data by date
     const groupedByDate = Array.from(
         d3.group(filteredData, (d) => d.parsedDate).values()
     );
     const groupedByTo = Array.from(d3.group(filteredData, (d) => d.to).values());
-    // var min = Math.min(...data.map(item => item.durationInTrafficMinutes));
-    // var max = Math.max(...data.map(item => item.durationInTrafficMinutes));
 
     // Prepare data for labels for today's date
     const labelData = groupedByTo
@@ -73,16 +88,18 @@ function createObservablePlotChart1(data, fromValue) {
         .map((group) => {
             const lastPoint = group[0];
             return {
-                parsedTime: Math.min(lastPoint.parsedTime, d3.timeParse("%H:%M:%S")(
-                    "20:00:00"
-                )),
-                kph: lastPoint.kph,
+                parsedTime: Math.min(
+                    lastPoint.parsedTime,
+                    d3.timeParse("%H:%M:%S")("20:00:00")
+                ),
+                y: lastPoint[field],
                 to: lastPoint.to,
-                ziel: lastPoint.ziel
+                ziel: lastPoint.ziel,
+                fromto: lastPoint.from_to,
             };
         });
     const chart = Plot.plot({
-        subtitle: "Start: " + fromValue,
+        subtitle: criteria,
         grid: true,
         x: {
             type: "utc",
@@ -90,21 +107,20 @@ function createObservablePlotChart1(data, fromValue) {
             tickFormat: d3.timeFormat("%H:%M"),
         },
         y: {
-            label: "Reisegeschwindigkeit (km/h)",
-            domain: [10, 50],
+            label: label,
+            domain: domain,
         },
         color: {
             type: "categorical",
             domain: ["Krumpaul", "Krankenhaus", "Höhle"],
-            range: ["darkorange", "#007bff", "green",],
+            range: ["darkorange", "#007bff", "green"],
         },
         marks: [
-
             ...groupedByDate.map((dayData) =>
                 Plot.line(dayData, {
                     curve: "monotone-x",
                     x: "parsedTime",
-                    y: "kph",
+                    y: field,
                     stroke: "to",
                     opacity: (d) => (d.is_today ? 1 : 0.12),
                 })
@@ -112,116 +128,46 @@ function createObservablePlotChart1(data, fromValue) {
 
             Plot.dot(filteredDataToday, {
                 x: "parsedTime",
-                y: "kph",
+                y: field,
                 fill: "to",
                 r: 2,
-                tip: true,
+                tip: true
             }),
 
-            Plot.text(labelData, {
-                x: "parsedTime",
-                y: "kph",
-                fill: "to",
-                text: "ziel",
-                fontSize: 18,
-                textAnchor: "start",
-                fontWeight: "bold",
-                // dy:
-                dx: 10, // Offset the label horizontally
-            }),
-        ],
-        width: 1360,
-        height: 425,
-    });
-    const div = document.querySelector("#chart_kph");
-    div.append(chart);
-
-    //    document.body.appendChild(chart);
-}
-
-function createObservablePlotChart2(data, fromValue) {
-    const filteredData = data.filter((d) => d.from === fromValue && d.parsedTime < d3.timeParse("%H:%M:%S")(
-        "22:00:00"
-    ));
-    const filteredDataToday = filteredData.filter((d) => d.is_today);
-
-    // Group data by date
-    const groupedByDate = Array.from(
-        d3.group(filteredData, (d) => d.parsedDate).values()
-    );
-    const groupedByTo = Array.from(d3.group(filteredData, (d) => d.to).values());
-    // var min = Math.min(...data.map(item => item.durationInTrafficMinutes));
-    // var max = Math.max(...data.map(item => item.durationInTrafficMinutes));
-
-    // Prepare data for labels for today's date
-    const labelData = groupedByTo
-        .filter((group) => group.some((d) => d.is_today))
-        .map((group) => {
-            const lastPoint = group[0];
-            return {
-                parsedTime: Math.min(lastPoint.parsedTime, d3.timeParse("%H:%M:%S")(
-                    "20:00:00"
-                )),
-                durationInTrafficMinutes: lastPoint.durationInTrafficMinutes,
-                to: lastPoint.to,
-                ziel: lastPoint.ziel
-            };
-        });
-    const chart = Plot.plot({
-        subtitle: "Start: " + fromValue,
-        grid: true,
-        x: {
-            type: "utc",
-            label: "Uhrzeit",
-            tickFormat: d3.timeFormat("%H:%M"),
-        },
-        y: {
-            label: "Reisedauer (min)",
-            // domain: [2, 8],
-        },
-        color: {
-            type: "categorical",
-            domain: ["Krumpaul", "Krankenhaus", "Höhle"],
-            range: ["darkorange", "#007bff", "green",],
-        },
-        marks: [
-            ...groupedByDate.map((dayData) =>
-                Plot.line(dayData, {
-                    curve: "monotone-x",
+            Plot.text(
+                labelData.filter((d) => d.to === "Höhle"),
+                {
                     x: "parsedTime",
-                    y: "durationInTrafficMinutes",
-                    stroke: "to",
-                    opacity: (d) => (d.is_today ? 1 : 0.12),
-                })
+                    y: "y",
+                    fill: "to",
+                    text: "fromto",
+                    fontSize: 18,
+                    textAnchor: "start",
+                    fontWeight: "bold",
+                    dy: -15,
+                    dx: 10, // Offset the label horizontally
+                }
             ),
-
-            Plot.dot(filteredDataToday, {
-                x: "parsedTime",
-                y: "durationInTrafficMinutes",
-                fill: "to",
-                r: 2,
-                tip: true,
-            }),
-
-            Plot.text(labelData, {
-                x: "parsedTime",
-                y: "durationInTrafficMinutes",
-                fill: "to",
-                text: "ziel",
-                fontSize: 18,
-                textAnchor: "start",
-                fontWeight: "bold",
-                // dy:
-                dx: 10, // Offset the label horizontally
-            }),
+            Plot.text(
+                labelData.filter((d) => d.to !== "Höhle"),
+                {
+                    x: "parsedTime",
+                    y: "y",
+                    fill: "to",
+                    text: "fromto",
+                    fontSize: 18,
+                    textAnchor: "start",
+                    fontWeight: "bold",
+                    dy: 0,
+                    dx: 10, // Offset the label horizontally
+                }
+            ),
         ],
         width: 1360,
         height: 425,
     });
-    const div = document.querySelector("#chart_time");
+    const div = document.querySelector(chartname);
     div.append(chart);
-
-    //    document.body.appendChild(chart);
 }
 
 function formatTimestamp(timestamp) {
